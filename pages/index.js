3ed75f13001d14396f097d431d7cd45ee333596e
Masaya Nakamura <m-nakamura@feedforce.jp>
2020-11-30 17:17:54 +0900

    Add a simple form with @shopify/react-form

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,48 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Page, Layout, Card, TextField } from "@shopify/polaris";
 import Link from "next/link";
+import { useField, useForm } from "@shopify/react-form";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-    <p>
-      <Link href="/another">
-        <a>Go to another page</a>
-      </Link>
-    </p>
-  </Page>
-);
+const Index = () => {
+  const { fields, dirty, submit, submitting } = useForm({
+    fields: {
+      text: useField({
+        value: "",
+      }),
+    },
+    async onSubmit(form) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log(form.text);
+      return { status: "success" };
+    },
+    makeCleanAfterSubmit: true,
+  });
+
+  return (
+    <Page>
+      <Layout>
+        <Layout.Section>
+          <Card
+            title="Form"
+            sectioned
+            primaryFooterAction={{
+              content: "Submit",
+              disabled: !dirty,
+              loading: submitting,
+              onAction: submit,
+            }}
+          >
+            <TextField label="Text" {...fields.text} />
+          </Card>
+
+          <Card sectioned>
+            <Link href="/another">
+              <a>Go to another page</a>
+            </Link>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+};
 
 export default Index;
