cbf44f3ee27bddd2a4b3c413499adb0c4f28b10d
Masaya Nakamura <m-nakamura@feedforce.jp>
2020-11-30 17:17:49 +0900

    Link the two pages

--- pages/another.js
+++ pages/another.js
@@ -1,8 +1,14 @@
 import { Heading, Page } from "@shopify/polaris";
+import Link from "next/link";
 
 const Another = () => (
   <Page>
     <Heading>This is another page!</Heading>
+    <p>
+      <Link href="/">
+        <a>Back to homepage</a>
+      </Link>
+    </p>
   </Page>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import { Heading, Page } from "@shopify/polaris";
+import Link from "next/link";
 
 const Index = () => (
   <Page>
     <Heading>Shopify app with Node and React 🎉</Heading>
+    <p>
+      <Link href="/another">
+        <a>Go to another page</a>
+      </Link>
+    </p>
   </Page>
 );
 
